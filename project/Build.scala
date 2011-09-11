d2828af6d2067e7b7b9c42ca9069a602ccb760e9
Paul Butcher <paul@paulbutcher.com>
2011-09-11 19:47:32 +0100

    Add root project

--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,8 @@
 import sbt._
 
 object MockBuild extends Build {
+  lazy val root = Project("root", file(".")) aggregate(subProject)
+  
   lazy val Mock = config("mock")
   lazy val mockSettings = inConfig(Mock)(Defaults.configSettings)
   lazy val subProject = Project("subProject", file("subProject")) settings(mockSettings: _*) configs(Mock)
