4a1404efbbe9ed55e88c7b574999a371898aa16b
Paul Butcher <paul@paulbutcher.com>
2011-09-11 19:44:25 +0100

    Move to sub project

--- project/Build.scala
+++ project/Build.scala
@@ -2,7 +2,6 @@ import sbt._
 
 object MockBuild extends Build {
   lazy val Mock = config("mock")
-  
   lazy val mockSettings = inConfig(Mock)(Defaults.configSettings)
-  lazy val root = Project("root", file(".")) settings(mockSettings: _*) configs(Mock)
+  lazy val subProject = Project("subProject", file("subProject")) settings(mockSettings: _*) configs(Mock)
 }
